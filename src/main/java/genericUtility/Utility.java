d170764bc543b4f62e4a440e45d81fa6cba8c8ec
HP <HP@DESKTOP-5P8GHBG>
2023-10-27 19:50:06 +0530

    add the utility

---
+++ src/main/java/genericUtility/Utility.java
@@ -0,0 +1,10 @@
+package genericUtility;
+
+public class Utility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("add some utility");
+	}
+
+}
